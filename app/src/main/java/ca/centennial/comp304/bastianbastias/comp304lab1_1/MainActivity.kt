62e5a4bf5b26c33d111e8ffe03bf253400d8c460
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-09-21 16:18:50 -0400

    added first and second activity interaction

--- app/src/main/java/ca/centennial/comp304/bastianbastias/comp304lab1_1/MainActivity.kt
+++ app/src/main/java/ca/centennial/comp304/bastianbastias/comp304lab1_1/MainActivity.kt
@@ -1,11 +1,30 @@
 package ca.centennial.comp304.bastianbastias.comp304lab1_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import ca.centennial.comp304.bastianbastias.comp304lab1_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val TAG = "MainActivity Lab 1 Exercise 1"
+        public const val MSG_KEY="MSG"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.e(TAG, "onCreate")
+
+        binding.button1.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            val data = binding.editText.text.toString()
+            intent.putExtra(MSG_KEY, data)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/ca/centennial/comp304/bastianbastias/comp304lab1_1/SecondActivity.kt
@@ -0,0 +1,18 @@
+package ca.centennial.comp304.bastianbastias.comp304lab1_1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ca.centennial.comp304.bastianbastias.comp304lab1_1.databinding.ActivitySecondBinding
+
+class SecondActivity:AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val extras = intent.extras
+        val text = if (extras != null) extras.get(MainActivity.MSG_KEY) as String else ""
+        setContentView(binding.root)
+        binding.textView1.text = text
+    }
+}
